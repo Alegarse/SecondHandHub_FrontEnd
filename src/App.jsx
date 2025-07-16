f3a5cdc4539701c944456689337e80b8be9c184b
Alejandro García Serrano <agscm2@gmail.com>
2025-07-16 17:27:20 +0200

    Upload timeless changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux'
+import './App.css'
+import store from './core/redux/store/store'
+import HomePage from './pages/homePage/HomePage'
+
 function App() {
 
   return (
-    <>
-    </>
+    <Provider store={store}>
+      <HomePage/>
+    </Provider>
   )
 }
 
