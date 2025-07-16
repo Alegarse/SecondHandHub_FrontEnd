7d8df6dd3ad2d0e40cb71d1b20ac915cda11f0b5
Alejandro García <agscm2@gmail.com>
2025-07-16 10:46:11 +0200

    Pre-Project. Initialice directories and react redux conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { Provider } from 'react-redux'
+import './App.css'
+import HomePage from './pages/HomePage/HomePage'
+import store from './core/redux/store/store'
+
 function App() {
 
   return (
-    <>
-    </>
+    <Provider store={store}>
+      <HomePage/>
+    </Provider>
   )
 }
 
