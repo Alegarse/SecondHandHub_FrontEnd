36ab56db31c89856904fbd98090105bb6128444c
Alejandro García <agscm2@gmail.com>
2025-07-31 12:52:46 +0200

    fix px to rem

--- src/components/EditProductComponent/EditProductComponent.jsx
+++ src/components/EditProductComponent/EditProductComponent.jsx
@@ -1,36 +1,33 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { getProductByIdFetch } from '../../core/services/productFetch';
-import { loadProductAction } from '../ProductComponent/ProductComponentActions';
-import { showToast } from '../../utils/utils';
-import ProductComponentLayout from '../ProductComponentLayout/ProductComponentLayout';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getProductByIdFetch } from "../../core/services/productFetch";
+import { loadProductAction } from "../ProductComponent/ProductComponentActions";
+import { showToast } from "../../utils/utils";
+import ProductComponentLayout from "../ProductComponentLayout/ProductComponentLayout";
 
 const EditProductComponent = () => {
+  const { productId } = useParams();
+  const dispatch = useDispatch();
 
-    const { productId } = useParams()
-  const dispatch = useDispatch()
-
-    const { dataProduct } = useSelector(
-    (state) => state.productComponentReducer
-  );
+  const { dataProduct } = useSelector((state) => state.productComponentReducer);
 
   useEffect(() => {
     const loadInitialData = async () => {
       try {
-        const productInfo = await getProductByIdFetch(productId)
+        const productInfo = await getProductByIdFetch(productId);
         dispatch(
           loadProductAction({
-            dataProduct: productInfo
+            dataProduct: productInfo,
           })
-        )
+        );
       } catch (error) {
         showToast("Error al cargar detalles del producto", "error");
         console.log(error.message);
       }
     };
     loadInitialData();
-  },[productId])
+  }, [productId]);
   return (
     <>
       <div className="details-product-container">
@@ -42,7 +39,7 @@ const EditProductComponent = () => {
       </div>
       <div className="toast-message" id="toastMessage"></div>
     </>
-  )
-}
+  );
+};
 
-export default EditProductComponent
+export default EditProductComponent;
